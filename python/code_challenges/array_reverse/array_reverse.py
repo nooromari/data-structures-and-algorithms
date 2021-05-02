e46c912080f730367107cae92efefb62e0763ff7
NOOR OMARI <nooromari847@gmail.com>
2021-05-03 02:24:53 +0300

    solved

---
+++ python/code_challenges/array_reverse/array_reverse.py
@@ -0,0 +1,10 @@
+
+def reverseArray(list):
+    newlist = []
+    for i in range(1, len(list) + 1):
+        newlist.append(list[-i])
+    return newlist
+
+
+testlist=[1, 2, 3, 4, 5, 6]
+print(reverseArray(testlist))
