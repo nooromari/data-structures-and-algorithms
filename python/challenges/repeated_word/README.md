cffde733d4877fe45a5cb23e829db511698c9916
NOOR OMARI <nooromari847@gmail.com>
2021-06-21 01:04:25 +0300

    code with test done

--- python/challenges/repeated_word/README.md
+++ python/challenges/repeated_word/README.md
@@ -6,8 +6,10 @@ Without utilizing any of the built-in library methods available to your language
 ## Whiteboard Process
 <!-- Embedded whiteboard image -->
 
+
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
 
 ## Solution
 <!-- Show how to run your code, and examples of it in action -->
--- python/tests/test_repeated_word.py
+++ python/tests/test_repeated_word.py
@@ -1,1 +1,12 @@
 from challenges.repeated_word.repeated_word import repeated_word
+import pytest
+
+def test_repeated_word():
+    test_str =["Once upon a time, there was a brave princess who...",
+"It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...",
+"It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."]
+    expected = ["a", "it", "summer"]
+    actual = [repeated_word(str) for str in test_str]
+    assert actual == expected
+
+
