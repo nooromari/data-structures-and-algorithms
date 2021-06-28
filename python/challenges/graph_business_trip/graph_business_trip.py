0f8fe08a73aa7a9fa3353930e22383830a5af333
NOOR OMARI <nooromari847@gmail.com>
2021-06-28 23:21:15 +0300

    ch37

--- python/challenges/graph_business_trip/graph_business_trip.py
+++ python/challenges/graph_business_trip/graph_business_trip.py
@@ -1,9 +1,11 @@
 def business_trip(gragh, cities):
-    """  """
+    """ return true if trip is possible with direct flights, and how much it would cost, otherwise return false. """
     cost = 0
-    flag = False
+    flag = True
 
     for i in range(len(cities)-1):
+        if not flag:
+            return False, '$0'
         neighbors = gragh.get_neighbors(cities[i])
         for neighbor in neighbors:
             if cities[i+1] == neighbor[0]:
@@ -14,4 +16,4 @@ def business_trip(gragh, cities):
                 flag = False
     if not flag :
         cost = 0
-    return flag, str(cost)+'$'
+    return flag, '$'+str(cost)
--- python/tests/test_graph_business_trip.py
+++ python/tests/test_graph_business_trip.py
@@ -1,1 +1,9 @@
 from challenges.graph_business_trip.graph_business_trip import business_trip
+import pytest
+
+def test_business_trip():
+    pass
+
+
+
+
